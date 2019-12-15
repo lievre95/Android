a01072ad18ac28c5ce10a3bc55d2fb815dd63e4b
dogajohn <ivandoga95@gmail.com>
2019-12-15 21:52:12 +0200

    first

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public void buttonisClicked(View buttonView){
+//        int result= 5*7;
+//        Log.i("OPERATIONAL", result +"");
+//        TextView txt = findViewById(R.id.text);
+//        Log.i("TEXT", txt.getText().toString());
+        EditText edtText = findViewById(R.id.edtText);
+//        Log.i("NAME", edtText.getText().toString());
+ EditText edtPhoneNumber = findViewById(R.id.edtPhoneNumber);
+// Log.i("PHONE", edtPhoneNumber.getText().toString());
+//        Toast.makeText(MainActivity.this,"This is TOAST message", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "Your name is: "+edtText.getText().toString()+"\n"+
+                "Your phone Number: "+ edtPhoneNumber.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
